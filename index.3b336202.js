feb15e7e7f7dd654fc8fac2b155ae175b4e0c082
katefranchuk <105155465+katefranchuk@users.noreply.github.com>
2022-11-24 17:26:08 +0000

    Deploying to gh-pages from @ katefranchuk/ice-cream-landing@6eb6e5c0ed0360f75acdf742c88cde63971bb461 🚀

---
+++ index.3b336202.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),new Swiper(".mySwipertwo",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.3b336202.js.map
