0ec9cf0dbb8aad224727781e9616610e359f0f31
Tetiana Novitska <novittana2410@gmail.com>
2022-11-27 19:48:47 +0200

    Add script to BtnByNow in menu

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-buy-now-open]'),
+    openModalInMenuBtn: document.querySelector('[data-modal-buy-now-open-in-menu]'),
     closeModalBtn: document.querySelector('[data-modal-buy-now-close]'),
     modalBuyNow: document.querySelector('[data-modal-buy-now]'),
     body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalInMenuBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
